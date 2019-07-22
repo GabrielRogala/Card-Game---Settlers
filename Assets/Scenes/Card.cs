bdb351b95b8e663d2ac1c7ccf8a6720df5d39b33
GabrielRogala <gabriel.rogala@gmail.com>
2019-07-22 22:22:14 +0200

    add card to asset menu

---
+++ Assets/Scenes/Card.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Basic")]
+public class Card : ScriptableObject
+{
+    public int cardId;
+    public string cardName;
+}
---
+++ Assets/Scenes/CardHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardHandler : MonoBehaviour
+{
+
+    public Card m_card;
+    public Text m_id;
+    public Text m_name;
+
+    void Start()
+    {
+        m_id.text = "#" + m_card.cardId;
+        m_name.text = m_card.cardName;
+    }
+
+}
