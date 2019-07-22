5d20e6834679a67b803dd48b9a7111bef6a59987
GabrielRogala <gabriel.rogala@gmail.com>
2019-07-22 21:28:50 +0200

    change the card's position to dropzone
    Card:
    - ability to change the card's position to dropzone

--- Assets/Scenes/Draggable.cs
+++ Assets/Scenes/Draggable.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public Transform m_parentToReturn = null;
 
+    GameObject m_placeholder = null;
+    LayoutElement m_layout = null;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        this.m_parentToReturn = this.transform.parent;
+        m_placeholder = new GameObject();
+        m_placeholder.transform.SetParent(this.transform.parent);
+
+        if (m_layout == null) {
+            m_layout = m_placeholder.AddComponent<LayoutElement>();
+        }
+
+        m_placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        m_parentToReturn = this.transform.parent;
         this.transform.SetParent(this.transform.parent.parent);
 
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -18,12 +31,33 @@ public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
+
+        Transform placeholderParent = m_placeholder.transform.parent;
+
+        int newSiblingIndex = placeholderParent.childCount;
+        for(int i = 0; i< placeholderParent.childCount; i++)
+        {
+            if(this.transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                if(m_placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                }
+                break;
+            }
+            
+        }
+
+        m_placeholder.transform.SetSiblingIndex(newSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        this.transform.SetParent(this.m_parentToReturn);
-
+        this.transform.SetParent(m_parentToReturn);
+        this.transform.SetSiblingIndex(m_placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(m_placeholder);
     }
 }
