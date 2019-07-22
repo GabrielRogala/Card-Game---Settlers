3370f7a7933002ae15e41fedd900a013d70ffaaf
GabrielRogala <gabriel.rogala@gmail.com>
2019-07-22 20:15:50 +0200

    create dropzone and card
    DropZone:
    - Layout
    - DropZonaHandler
      - onDropEvent
    
    Card:
    - Draggable
      - onDrag

---
+++ Assets/Scenes/DropZoneHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZoneHandler : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was drop on " + gameObject.name);
+    }
+}
