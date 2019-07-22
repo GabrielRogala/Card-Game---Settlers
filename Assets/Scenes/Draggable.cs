31a035a56a472beab108db87f41c62c49ae6833c
GabrielRogala <gabriel.rogala@gmail.com>
2019-07-22 21:10:28 +0200

    Drag&Drop
    Card:
    - return to the previous dropzone if you do not drop it in any dropzone
    - can be dropped between the dropzones

--- Assets/Scenes/Draggable.cs
+++ Assets/Scenes/Draggable.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IDragHandler
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    public Transform m_parentToReturn = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        this.m_parentToReturn = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(this.m_parentToReturn);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
--- Assets/Scenes/DropZoneHandler.cs
+++ Assets/Scenes/DropZoneHandler.cs
@@ -8,5 +8,11 @@ public class DropZoneHandler : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log(eventData.pointerDrag.name + " was drop on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.m_parentToReturn = this.transform;
+        }
     }
 }
